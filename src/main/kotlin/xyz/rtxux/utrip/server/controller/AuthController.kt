aee8285d7978447ce9be87e4c558838281dd804b
RTXUX <wyf@rtxux.xyz>
2019-11-07 02:05:29 +0800

    Implement basic register

--- src/main/kotlin/xyz/rtxux/utrip/server/controller/AuthController.kt
+++ src/main/kotlin/xyz/rtxux/utrip/server/controller/AuthController.kt
@@ -3,21 +3,27 @@ package xyz.rtxux.utrip.server.controller
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import xyz.rtxux.utrip.server.exception.AppException
 import xyz.rtxux.utrip.server.model.dto.LoginDTO
 import xyz.rtxux.utrip.server.model.dto.RegisterDTO
+import xyz.rtxux.utrip.server.model.po.User
 import xyz.rtxux.utrip.server.model.vo.ApiResponseVO
 import xyz.rtxux.utrip.server.model.vo.LoginVO
 import xyz.rtxux.utrip.server.model.vo.RegisterVO
+import xyz.rtxux.utrip.server.repository.UserRepsitory
 
 @RestController
 @RequestMapping("/auth")
 @Api(tags = arrayOf("认证"))
 class AuthController @Autowired constructor(
-
+        private val userRepsitory: UserRepsitory,
+        private val passwordEncoder: PasswordEncoder
 ) {
     @ApiOperation("登录")
     @PostMapping("/login")
@@ -25,7 +31,20 @@ class AuthController @Autowired constructor(
 
     @ApiOperation("注册")
     @PostMapping("/register")
-    fun register(@RequestBody registerDTO: RegisterDTO): ApiResponseVO<RegisterVO>? = null
+    @Transactional
+    fun register(@RequestBody registerDTO: RegisterDTO): ApiResponseVO<RegisterVO> {
+        if (userRepsitory.findByUsername(registerDTO.username).isPresent) {
+            throw AppException(1001, "用户已存在")
+        }
+        val user = User(
+                username = registerDTO.username,
+                password = passwordEncoder.encode(registerDTO.password),
+                nickname = registerDTO.nickname,
+                gender = "无"
+        )
+        val savedUser = userRepsitory.save(user)
+        return ApiResponseVO(0, "Success", RegisterVO(savedUser.id!!, savedUser.username!!))
+    }
 
     @ApiOperation("注销")
     @PostMapping("/logout")

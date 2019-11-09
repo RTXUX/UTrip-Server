cb561d6e8fcfe13ff508f33570fad880da5ec941
RTXUX <wyf@rtxux.xyz>
2019-11-10 02:18:02 +0800

    New API for validate login runtime credential

--- src/main/kotlin/xyz/rtxux/utrip/server/controller/AuthController.kt
+++ src/main/kotlin/xyz/rtxux/utrip/server/controller/AuthController.kt
@@ -3,12 +3,10 @@ package xyz.rtxux.utrip.server.controller
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import xyz.rtxux.utrip.server.exception.AppException
 import xyz.rtxux.utrip.server.model.dto.LoginDTO
 import xyz.rtxux.utrip.server.model.dto.RegisterDTO
@@ -17,6 +15,7 @@ import xyz.rtxux.utrip.server.model.vo.ApiResponseVO
 import xyz.rtxux.utrip.server.model.vo.LoginVO
 import xyz.rtxux.utrip.server.model.vo.RegisterVO
 import xyz.rtxux.utrip.server.repository.UserRepsitory
+import xyz.rtxux.utrip.server.service.impl.MyUserDetails
 
 @RestController
 @RequestMapping("/auth")
@@ -49,4 +48,13 @@ class AuthController @Autowired constructor(
     @ApiOperation("注销")
     @PostMapping("/logout")
     fun logout(): ApiResponseVO<Unit>? = null
+
+    @ApiOperation("验证登录有效性")
+    @GetMapping("/validate")
+    fun validate(@AuthenticationPrincipal user: MyUserDetails?): ApiResponseVO<LoginVO> {
+        if (user == null) {
+            return ApiResponseVO(1001, "Not authenticated")
+        }
+        return ApiResponseVO(0, "Success", LoginVO(user.userId, user.username))
+    }
 }
